925c3de9f0ee7487f421487fc13208eba7196dff
Harsh Sandhu <harsh1739371@gmail.com>
2025-04-04 10:50:22 +0530

    Add dashboard layout and components, including skeleton, site header, and navigation elements

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+import { AppSidebar } from "@/components/dashboard/app-sidebar";
+import { SiteHeader } from "@/components/dashboard/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
+export default function DashbaordLayout({
+  children,
+} : React.PropsWithChildren) {
+  return (
+    <SidebarProvider>
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+        {children}
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
